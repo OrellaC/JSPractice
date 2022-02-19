424727f0f9ab2b0d5a04753d09620a81f32a4495
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-18 22:58:28 -0500

    Completed Data types and let, const, and var lecture

--- ValuesAndVariables/ValuesandVar.js
+++ ValuesAndVariables/ValuesandVar.js
@@ -2,7 +2,20 @@
 // 2. Log their values to the console
 
 let country = "Guyana";
-let continent = "South America";
+const continent = "South America";
 let population = "786,559";
 
 console.log(country, continent, population);
+
+
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console.
+let isIsland = false;
+
+// Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
+let language = "English"; 
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
