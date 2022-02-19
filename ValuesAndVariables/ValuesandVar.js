7883321d7f07249ad0fbb92dbabc40148f7f053d
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-18 22:46:20 -0500

    Completes Values and Variables Lecture

---
+++ ValuesAndVariables/ValuesandVar.js
@@ -0,0 +1,8 @@
+// Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Guyana";
+let continent = "South America";
+let population = "786,559";
+
+console.log(country, continent, population);
