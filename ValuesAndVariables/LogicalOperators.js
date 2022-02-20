36d6a66933e5eac0ecf038e6b60f1fec66e6e8bb
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-19 22:58:33 -0500

    Completed Logical Operators lecture

--- ValuesAndVariables/LogicalOperators.js
+++ ValuesAndVariables/LogicalOperators.js
@@ -1,5 +1,16 @@
 // Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks english, has less than 50 million people and is not an island.
+
+let language = "English";
+population = 50000000;
+country = "Guyana"; 
 // 3. Write an if statement to help Sarah figure out if your country is right for her.
 // You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.
 // 4. If yours is the right country, log a string like this: 'You should live in Portugal :)'. If not, log 'Portugal does not meet your criteria :('
 // 5. Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D)
+
+
+if (language === 'English' && population < 50000000 && !isIsland){
+console.log(`You should live in ${country}`)
+} else {
+    console.log(`${country} does not meet your criteria`)
+}
