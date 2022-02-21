1f389c5f2374008b93a7d39a2a20794b79c1bd40
orellachichester@gmail.com <orellachichester@gmail.com>
2022-02-20 22:20:59 -0500

    Completed Objects Lecture

---
+++ ValuesAndVariables/FunctionDeclarations.js
@@ -0,0 +1,3 @@
+// ===== Function Declaration vs. Expressions ====
+// The world population is 7900 million people. Create a function declaration called 'percentageOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population
+
---
+++ ValuesAndVariables/Objects.js
@@ -0,0 +1,10 @@
+// =========== Objects ============
+// Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+    country = "Guyana",
+    capital = "Gerogetown",
+    language = "English",
+    population = 700000,
+    neighbors =["Venezula", "Suriname", "Brazil"]
+}
